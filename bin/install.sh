96b2ff74de9847bcb99ea67b13ee900fe1144d12
Steve Hannah <steve@weblite.ca>
2019-09-29 07:53:27 -0700

    Moved install.sh to setup.sh since that better reflects what it does.  Created install script that can be called to download and install swete to central location.

--- bin/install.sh
+++ bin/install.sh
@@ -1,71 +1,16 @@
-#!/bin/sh
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+#!/bin/bash
 set -e
-if [ ! -d "../swete-admin/templates_c" ]; then
-	cd ../swete-admin
-	mkdir templates_c
-	
+SWETE_ROOT=$HOME/.swete
+SWETE_BIN=$SWETE_ROOT/bin
+if [ ! -d "$SWETE_ROOT" ]; then
+	cd $HOME
+	git clone https://github.com/shannah/swete .swete
+	cd .swete/bin
+	sh setup.sh
 fi
-if [ ! -d "../swete-admin/xataface" ]; then
-    cd ../swete-admin
-    echo "Downloading Xataface..."
-    curl -Ls https://github.com/shannah/xataface/archive/master.zip > xataface.zip
-    echo "Extracting Xataface..."
-    unzip -qq xataface.zip
-	rm xataface.zip
-    mv xataface-master xataface
-    cd ../bin
+if [[ ":$PATH:" == *":$SWETE_BIN:"* ]]; then
+	echo "export PATH=$SWETE_BIN:$PATH" >> $HOME/.bash_profile
+else
+  echo "Your path is missing ~/bin, you might want to add it."
 fi
-if [ ! -d "../swete-admin/app" ]; then
-    cd ../swete-admin
-    echo "Downloading SweteApp..."
-    curl -Ls https://github.com/shannah/swete-app/raw/master/SweteApp.zip > app.zip
-    echo "Extracting SweteApp..."
-    mkdir app
-	mv app.zip app/app.zip
-	cd app
-	unzip -qq app.zip
-	rm app.zip
-    cd ../../bin
-fi
-if [ ! -f "../swete-admin/modules/ckeditor/ckeditor.php" ]; then
-	if [ -d "../swete-admin/modules/ckeditor" ]; then
-		rm -rf "../swete-admin/modules/ckeditor"
-	fi
-    cd ../swete-admin/modules
-    echo "Downloading ckeditor module..."
-    curl -Ls https://github.com/shannah/xataface-module-ckeditor/archive/master.zip > ckeditor.zip
-    echo "Extracting ckeditor module..."
-    unzip -qq ckeditor.zip
-    mv xataface-module-ckeditor-master ckeditor
-	rm ckeditor.zip
-    cd ../../bin
-fi
-if [ ! -f "../swete-admin/modules/uitk/uitk.php" ]; then
-	if [ -d "../swete-admin/modules/uitk" ]; then
-		rm -rf "../swete-admin/modules/uitk"
-	fi
-    cd ../swete-admin/modules
-    echo "Downloading uitk module..."
-    curl -Ls https://github.com/shannah/xataface-module-uitk/archive/master.zip > uitk.zip
-    echo "Extracting uitk module..."
-    unzip -qq uitk.zip
-    mv xataface-module-uitk-master uitk
-	rm uitk.zip
-    cd ../../bin
-fi
-if [ ! -f "../swete-admin/modules/excel/excel.php" ]; then
-	if [ -d "../swete-admin/modules/excel" ]; then
-		rm -rf "../swete-admin/modules/excel"
-	fi
-    cd ../swete-admin/modules
-    echo "Downloading excel module..."
-    curl -Ls https://github.com/shannah/xataface-module-excel/archive/master.zip > excel.zip
-    echo "Extracting excel module..."
-    unzip -qq excel.zip
-    mv xataface-module-excel-master excel
-	rm excel.zip
-    cd ../../bin
-fi
-
 
